aed6e570ec2bfb407cf6135f7b8ec18ecb55e494
Mary <mrood@wisc.edu>
2015-05-20 18:33:35 -0500

    modified script for new datasets

--- makeSumMaster.py
+++ makeSumMaster.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+sampleFile = raw_input("sample file path: ")
+parameters = raw_input("parameters, i.e. mq20, mq20_mc3, mq30: ")
 outfileName = raw_input("outfile name: ")
 
 def make_geneList():
@@ -12,17 +14,25 @@ def make_geneList():
 
 def make_geneDict():
     geneDict = {}
-    with open("/home/mrood/PerGeneSummary.txt", 'r') as infile:
+    with open("/home/mrood/scripts/importantFiles/PerGeneSummary.txt", 'r') as infile:
         for line in infile:
             line = line.strip().split('\t')
             geneDict[line[0]] = [line[1], line[3], line[7]]
-    print geneDict["Rv0001"]
+    #print geneDict["Rv0001"] #test to make sure generated 
     return geneDict
 
-def make_dicts(geneList):
+def make_sampleList(sampleFile):
+    samples = []
+    with open(sampleFile, 'r') as infile:
+        for line in infile:
+            line = line.strip()
+            samples.append(line)
+    print('There are {0} samples to be processed.'.format(len(samples)))
+    return samples
+
+def make_dicts(geneList, samples):
     d = {}
     stats = ["theta", "pi", "TD", "piNpiS"]
-    samples = ["a1", "a2", "a3", "b2", "c1", "c2"]
     for sample in samples:
         d[sample] = {}
         for stat in stats:
@@ -43,7 +53,7 @@ def make_dicts(geneList):
     return d
 
 def pop_dg(d):
-    with open("/home/mrood/data/Mtb/141107/global/bygene/global.genes.tajD", 'r') as file:
+    with open("global/global.genes.tajD", 'r') as file:
         for line in file:
             line = line.strip().split('\t')
             gene = line[0]
@@ -51,9 +61,9 @@ def pop_dg(d):
                 cov = float(line[2])
             except ValueError:
                 cov = line[2]
-            if type(cov) == float and cov >= 0.55:
+            if type(cov) == float and cov >= 0.5:
                 d["global"]["TD"][gene] = line[3]
-    with open("/home/mrood/data/Mtb/141107/global/bygene/global.genes.pi", 'r') as file:
+    with open("global/global.genes.pi", 'r') as file:
         for line in file:
             line = line.strip().split('\t')
             gene = line[0]
@@ -61,10 +71,10 @@ def pop_dg(d):
                 cov = float(line[2])
             except ValueError:
                 cov = line[2]
-            if type(cov) == float and cov >= 0.55:
+            if type(cov) == float and cov >= 0.5:
                 d["global"]["pi"][gene] = line[3]
     passGenes = []
-    with open("/home/mrood/data/Mtb/141107/global/bygene/global.genes.theta", 'r') as file:
+    with open("global/global.genes.theta", 'r') as file:
         for line in file:
             line = line.strip().split('\t')
             gene = line[0]
@@ -72,11 +82,11 @@ def pop_dg(d):
                 cov = float(line[2])
             except ValueError:
                 cov = line[2]
-            if type(cov) == float and cov >= 0.55:
+            if type(cov) == float and cov >= 0.5:
                 d["global"]["theta"][gene] = line[1:]
                 passGenes.append(gene)
-    print("global sample has " + str(len(passGenes)) + " genes with >= 0.55 coverage")
-    with open("/home/mrood/data/Mtb/141107/global/syn-nonsyn/global_final_syn-nonsyn.piNpiS", 'r') as infile:
+    print("global sample has " + str(len(passGenes)) + " genes with >= 0.5 coverage")
+    with open("global/global_syn-nonsyn.pi", 'r') as infile:
         for line in infile:
             line = line.strip().split('\t')
             gene = line[0]
@@ -91,18 +101,11 @@ def pop_dg(d):
                 else:
                     NS = float(line[5])/float(line[6])
                 d["global"]["piNpiS"][gene] = [line[1],line[2],line[3],line[4],line[5],line[6],str(NS)]
-    with open("/home/mrood/data/Mtb/141107/global/global-Rv2946c_syn-nonsyn.pi", 'r') as infile2:
-        for line in infile2:
-            line = line.strip().split()
-            gene = "Rv2946c"
-            if gene in passGenes:
-                d["global"]["piNpiS"]["Rv2946c"] = [line[1],line[2],line[3],line[4],line[5],line[6],str((float(line[5])/float(line[6])))]
     return d
 
-def pop_di(d):
-    sampleList = ["a1", "a2", "a3", "b2", "c1", "c2"]
-    for sample in sampleList:
-        with open('/home/mrood/data/Mtb/141107/intrahost/subsampled/50X/bygene/tajD/average/' + sample + '_tajD.genes.mean', 'r') as file:
+def pop_di(d, samples):
+    for sample in samples:
+        with open('tajD/' + sample + '_' + parameters + '_tajD.genes.mean', 'r') as file:
             next(file)
             for line in file:
                 line = line.strip().split('\t')
@@ -111,9 +114,9 @@ def pop_di(d):
                     cov = float(line[1])
                 except ValueError:
                     cov = line[1]
-                if type(cov) == float and cov >= 0.55:
+                if type(cov) == float and cov >= 0.5:
                     d[sample]["TD"][gene] = line[12]
-        with open('/home/mrood/data/Mtb/141107/intrahost/subsampled/50X/bygene/pi/average/' + sample + '_pi.genes.mean', 'r') as infile:
+        with open('pi/' + sample + '_' + parameters + '_pi.genes.mean', 'r') as infile:
             next(infile)
             for line in infile:
                 line = line.strip().split('\t')
@@ -122,10 +125,10 @@ def pop_di(d):
                     cov = float(line[1])
                 except ValueError:
                     cov = line[1]
-                if type(cov) == float and cov >= 0.55:
+                if type(cov) == float and cov >= 0.5:
                     d[sample]["pi"][gene] = line[12]
         passGenes = []
-        with open("/home/mrood/data/Mtb/141107/intrahost/subsampled/50X/bygene/theta/average/" + sample + '_theta.genes.mean', 'r') as infile3:
+        with open("theta/" + sample + '_' + parameters + '_theta.genes.mean', 'r') as infile3:
             for line in infile3:
                 line = line.strip().split('\t')
                 gene = line[0]
@@ -133,37 +136,56 @@ def pop_di(d):
                     cov = float(line[1])
                 except ValueError:
                     cov = line[1]
-                if type(cov) == float and cov >= 0.55:
+                if type(cov) == float and cov >= 0.5:
                     d[sample]["theta"][gene] = [line[1],line[12]]
                     passGenes.append(gene)
-        print(sample + " has " + str(len(passGenes)) + " genes with >= 0.55 coverage")
-        with open('/home/mrood/data/Mtb/141107/intrahost/subsampled/50X/syn-nonsyn/avg/' + sample + '_syn-nonsyn.avg', 'r') as infile4:
+        print(sample + " has " + str(len(passGenes)) + " genes with >= 0.5 coverage")
+        #with open('piNpiS/' + sample + '_med.calc', 'r') as infile4:
+        #    next(infile4)
+        #    for line in infile4:
+        #        line = line.strip().split('\t')
+        #        gene = line[0]
+        #        if gene in passGenes:
+        #            d[sample]["piNpiS"][gene] = [line[1],line[3],line[5]]
+        with open('piNpiS/' + sample + '_' + parameters + '_med.calc', 'r') as infile4:
             next(infile4)
             for line in infile4:
                 line = line.strip().split('\t')
                 gene = line[0]
-                if gene in passGenes:
-                    d[sample]["piNpiS"][gene] = [line[1],line[3],line[5]]
-        with open('/home/mrood/data/Mtb/141107/intrahost/subsampled/50X/pileups/Rv2946c/Rv2946c.all.piNpiS', 'r') as infile5:
-            for line in infile5:
-                line = line.strip().split()
-                gene = 'Rv2946c'
-                sample = line[0].split("-")[0]
-                if gene in passGenes:
-                    d[sample]["piNpiS"][gene] = [line[1], line[3], line[5]]
+                if gene in passGenes and line[1] != "NA":
+                    if float(line[1]) == 0:
+                        if float(line[2]) == 0:
+                            NS = "NaN"
+                        else:
+                            NS = "DQ"
+                    elif float(line[2]) == 0:
+                        NS = "Inf"
+                    else:
+                        NS = float(line[1])/float(line[2])
+                    d[sample]["piNpiS"][gene] = [line[1],line[2],str(NS)]
     return d
 
 
-def write_files(geneList, geneDict, d):
-    samples = ["global","a1","a2","a3","b2","c1","c2"]
+def write_files(geneList, geneDict, d, samples):
     with open(outfileName, 'w') as outfile:
         outfile.write(
-        'gene.symbol\tgene.name\tgene.length\tgene.des\tg.snps\tg.coverage\tg.theta\tg.pi\tg.TD\tg.NSlen\tg.Slen\tg.NSsnps\tg.Ssnps\tg.piN\tg.piS\tg.piN.piS\ta1.coverage\ta1.theta\ta1.pi\ta1.TD\ta1.piN\ta1.piS\ta1.piN.piS\ta2.coverage\ta2.theta\ta2.pi\ta2.TD\ta2.piN\ta2.piS\ta2.piN.piS\ta3.coverage\ta3.theta\ta3.pi\ta3.TD\ta3.piN\ta3.piS\ta3.piN.piS\tb2.coverage\tb2.theta\tb2.pi\tb2.tajD\tb2.piN\tb2.piS\tb2.piN.piS\tc1.coverage\tc1.theta\tc1.pi\tc1.tajD\tc1.piN\tc1.piS\tc1.piN.piS\tc2.coverage\tc2.theta\tc2.pi\tc2.tajD\tc2.piN\tc2.piS\tc2.piN.piS\n'
-        )
+        'gene.symbol\tgene.name\tgene.length\tgene.des\tg.snps\tg.coverage\tg.theta\tg.pi\tg.TD\tg.NSlen\tg.Slen\tg.NSsnps\tg.Ssnps\tg.piN\tg.piS\tg.piN.piS\t')
+        for sample in samples:
+            outfile.write("%s\t%s\t%s\t%s\t%s\t%s\t%s\t" %
+            (sample+'.cov',
+            sample+'.theta',
+            sample+'.pi',
+            sample+'.tajD',
+            sample+'.piN',
+            sample+'.piS',
+            sample+'.piNpiS'))
+        
+        #a1.coverage\ta1.theta\ta1.pi\ta1.TD\ta1.piN\ta1.piS\ta1.piN.piS\ta2.coverage\ta2.theta\ta2.pi\ta2.TD\ta2.piN\ta2.piS\ta2.piN.piS\ta3.coverage\ta3.theta\ta3.pi\ta3.TD\ta3.piN\ta3.piS\ta3.piN.piS\tb2.coverage\tb2.theta\tb2.pi\tb2.tajD\tb2.piN\tb2.piS\tb2.piN.piS\tc1.coverage\tc1.theta\tc1.pi\tc1.tajD\tc1.piN\tc1.piS\tc1.piN.piS\tc2.coverage\tc2.theta\tc2.pi\tc2.tajD\tc2.piN\tc2.piS\tc2.piN.piS\n')
         for gene in geneList:
             try:
                 outfile.write(gene + '\t' + "\t".join(geneDict[gene]) + '\t')
             except KeyError:
+                print('KeyError: ' + gene)
                 outfile.write(gene + '\t\t\t')
             for sample in samples:
                 outfile.write("%s\t%s\t%s\t%s\t" %
@@ -176,7 +198,8 @@ def write_files(geneList, geneDict, d):
 
 geneList = make_geneList()
 geneDict = make_geneDict()
-d = make_dicts(geneList)
+samples = make_sampleList(sampleFile)
+d = make_dicts(geneList, samples)
 dg = pop_dg(d)
-dgi = pop_di(dg)
-write_files(geneList, geneDict, dgi)
+dgi = pop_di(dg, samples)
+write_files(geneList, geneDict, dgi, samples)
